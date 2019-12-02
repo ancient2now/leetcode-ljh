69fe5993971423dee844c12130ced44c08068b45
JIEUN <ljh@JIEUNdeMacBook-Pro.local>
2019-12-02 21:12:52 +0900

    circular left shift N steps in an integer array

---
+++ GraduateEntranceExamination/circular-left-shift.c
@@ -0,0 +1,56 @@
+/* 设将n(n,1)个整数存放到一维数组R中，试设计一个在时间和空间两方面尽可能有效的算法，将R中保有的序列循环左移P（0﹤P﹤n）个位置，即将R中的数据由（X0 X1 ……Xn-1）变换为（Xp Xp+1 ……Xn-1 X0 X1 ……Xp-1）要求：
+
+　　（1）给出算法的基本设计思想。
+    offset:3
+    step1:1 2 3 | 4 5 6 7
+    step2:3 2 1 | 7 6 5 4
+    step3:4 5 6 7 | 1 2 3
+
+　　（2）根据设计思想，采用C或C++或JAVA语言表述算法，关键之处给出注释。
+
+　　（3）说明你所设计算法的时间复杂度和空间复杂度
+    O(N)time O(1)space
+*/
+#include <stdio.h>
+
+void reverse(int * arr, int from, int to);
+void print_array(int * arr, int len);
+void converse(int * arr, int len, int offset);
+int main(int argc, char const *argv[])
+{
+    int arr[] = {1,2,3,4,5,6,7};
+    int len = sizeof(arr) / sizeof(int);
+    print_array(arr, len);
+    int offset = 7;
+
+    converse(arr, len, offset);
+    print_array(arr, len);
+    return 0;
+}
+
+void converse(int * arr, int len, int offset){
+    offset = offset % len;
+    if(offset == 0)
+        return;
+    reverse(arr, 0 , offset-1);
+    reverse(arr, offset , len-1);
+    reverse(arr, 0 , len-1);
+}
+
+void reverse(int * arr, int from, int to){
+    int i,temp;
+    for(i = 0; i < (to-from)/2; i++){
+        temp = arr[from+i];
+        arr[from+i] = arr[to-i];
+        arr[to-i] = temp;
+    }
+}
+void print_array(int * arr, int len){
+    for(int i = 0; i < len; ++i){
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+
+
